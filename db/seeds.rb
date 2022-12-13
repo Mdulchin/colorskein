4190038b9abab8f49ad12a19b8f7af8eb44c16fa
madelinedulchin <madeline.dulchin@gmail.com>
2022-12-13 16:40:53 -0500

    gotta do it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(username: "mad", email: "mad@gmail.com", password: "12345")
+user2 = User.create(username: "ang", email: "ang@gmail.com", password: "12345")
+user3 = User.create(username: "ale", email: "ale@gmail.com", password: "12345")
