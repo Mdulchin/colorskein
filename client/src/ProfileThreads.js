6c68446bed0e1f131fa5937ff6dfb08f60b02235
madelinedulchin <madeline.dulchin@gmail.com>
2022-12-20 16:11:49 -0500

    my projectinging

--- client/src/ProfileThreads.js
+++ client/src/ProfileThreads.js
@@ -1,18 +1,25 @@
-
+import { useState} from 'react'
+import { Link } from 'react-router-dom'
 function ProfileThreads({floss}){
+// const [hasFlosses, setHasFlosses] = useState([currentUser.flosses])
+
     function removeThread(e) {
         fetch(`/user_flosses/${floss.id}`, {
             method: 'DELETE',
         }).then(r => {
             if (r.ok) {
                 console.log(r)
-
             }
         })
         e.target.parentNode.remove()
     }
+
+    
         return (
             <div className="flossCard">
+                {/* <Link to='/floss'>
+        <button className="floss">Add Threads</button>
+            </Link> */}
                 <h2>{floss.dmc_name}</h2>
           <img src={floss.image}></img>
           <button onClick={(e) => removeThread(e)}>Remove from my threads</button>
