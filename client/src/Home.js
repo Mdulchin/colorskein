9888f616e88482d3619da49b257f5d6bdcd94e4e
madelinedulchin <madeline.dulchin@gmail.com>
2023-01-03 12:26:58 -0500

    home page is cute!

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,6 +1,7 @@
 import Floss from "./Floss";
 import AllThreads from "./AllThreads";
 import {useState, useEffect } from "react";
+import homepagegraphic from "./finalblueberrys.png"
 
 function Home({flossArray}){
 const [flossIndex, setFlossIndex] = useState(0)
@@ -27,6 +28,10 @@ return (
     {flossIndex > 0 ? <button className="prevFloss" onClick={() => setFlossIndex(flossIndex - 12)}>⇦</button> : null}
     {flossPhotos} 
     {flossIndex < flossArray.length ? <button className="nextFloss" onClick={() => setFlossIndex(flossIndex + 12)}>⇨</button> : null}
+    <br />
+    <h3 className="homeDescription">Use ColorSkein to help you choose the best <br /> DMC embroidery floss colors for your project!</h3>
+    <br />
+    <img className="homeImage"src={homepagegraphic}></img>
     </span>
  
 
